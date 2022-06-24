5b61686de67b3ba2213087460b0fb058321e330e
Tanuj-Mishra <tanujtmmishra@gmail.com>
2022-06-24 11:28:07 +0530

    Added 1.3.1.3

--- 1.3.1.3 Climb Stairs With Variable Jumps/2. Solution/3. Code/ClimbStairsVariableJump.java
+++ 1.3.1.3 Climb Stairs With Variable Jumps/2. Solution/3. Code/ClimbStairsVariableJump.java
@@ -5,23 +5,23 @@ class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
-        int[] arr = new int[n];
+        int[] jumps = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = scn.nextInt();
+            jumps[i] = scn.nextInt();
         }
 
-        System.out.println(climbStairVJ(n, arr));
-
+        System.out.println(climbStairVJ(n, jumps));
+        scn.close();
     }
 
     // returns total number of path from 0 to nth floor with varisolutionBankle
     // jumps
-    public static int climbStairVJ(int n, int[] arr) {
+    public static int climbStairVJ(int n, int[] jumps) {
 
         int[] solutionBank = new int[n + 1];
         solutionBank[n] = 1;
         for (int i = n - 1; i >= 0; i--) {
-            for (int j = 1; j <= arr[i] && i + j < solutionBank.length; j++) {
+            for (int j = 1; j <= jumps[i] && i + j < solutionBank.length; j++) {
                 solutionBank[i] += solutionBank[i + j];
             }
         }

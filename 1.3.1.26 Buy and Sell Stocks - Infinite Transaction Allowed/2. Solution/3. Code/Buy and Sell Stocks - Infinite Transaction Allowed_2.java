f89b7bdb30e0d4cb7a31c166d5d5663cc9d7c1cf
Tanuj-Mishra <tanujtmmishra@gmail.com>
2022-07-03 16:40:50 +0530

    Added Method-2 for 1.3.1.26

---
+++ 1.3.1.26 Buy and Sell Stocks - Infinite Transaction Allowed/2. Solution/3. Code/Buy and Sell Stocks - Infinite Transaction Allowed_2.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] prices = new int[n];
+        for(int i = 0; i < n; i++) {
+            prices[i] = scn.nextInt();
+        }
+
+        int[] dp = new int[n];
+        int buy = prices[0];
+        int sell = prices[0];
+        int overallProfit = 0;
+
+        for(int i = 1; i < n; i++) {
+            if(prices[i] >= prices[i-1]) {
+                sell = prices[i];
+            } else {
+                overallProfit += sell - buy;               
+                buy = sell = prices[i];
+            }
+        }
+        overallProfit += sell - buy;               
+
+        System.out.println(overallProfit);
+        
+    }
+
+}
+
+
